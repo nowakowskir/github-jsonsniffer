f242bddd5364044d7939b9de53a00e8e1e82fa8c
Radoslaw Nowakowski <nowakowski.r@gmail.com>
2023-10-06 16:18:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { spawn } = require('child_process');
+const { createInterface }  = require('readline');
+const path = require('path');
+const picomatch = require('picomatch');
+const { existsSync } = require('fs');
+
+async function run() {
+    try {
+        const matches = picomatch('**.json');
+
+        console.log(JSON.stringify(github.context.payload, undefined, 2));
+
+        const diffProcess = spawn('git',
+            [
+                '--no-pager',
+                'diff-tree',
+                '--no-commit-id',
+                '--name-status',
+                '--diff-filter=d',
+                '-r',
+                `${github.context.payload.pull_request.base.sha}..`
+            ],
+            {
+                windowsHide: true,
+                timeout: 10000,
+            }
+        );
+
+        const lines = createInterface({
+            input: diffProcess.stdout,
+        });
+
+        const affectedFiles = {
+            added: [],
+            modified: [],
+        };
+
+        const matchersPath = path.join(__dirname, '.', '.github');
+
+        console.log(
+            `##[add-matcher]${path.join(matchersPath, 'phpcs-matcher.json')}`
+        );
+
+        for await (const line of lines) {
+            const parsed = /^(?<status>[ACMR])[\s\t]+(?<file>\S+)$/.exec(line);
+            if (parsed.groups) {
+                const { status, file } = parsed.groups;
+                if (matches(file) && existsSync(file)) {
+                    switch (status) {
+                        case 'A':
+                        case 'C':
+                        case 'R':
+                            affectedFiles.added.push(file);
+                            break;
+                        case 'M':
+                            affectedFiles.modified.push(file);
+                    }
+                }
+            }
+        }
+
+        if (affectedFiles.added.length === 0 && affectedFiles.modified.length === 0) {
+            core.warning('Empty list of files, nothing to process!');
+
+            return;
+        }
+
+        // if (affectedFiles.added.length > 0) {
+        //
+        // }
+
+        console.log(affectedFiles.added);
+        console.log(affectedFiles.modified);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
